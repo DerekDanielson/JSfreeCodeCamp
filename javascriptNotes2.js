08934ddd41692869631e9b32c1fc89b086bccb7d
Derek Danielson <derekdanielson84@gmail.com>
2022-04-28 10:10:55 -0600

    More JS notes

---
+++ javascriptNotes2.js
@@ -0,0 +1,23 @@
+//ESCAPE SEQUENCES IN STRINGS
+\'      ---- single quote
+\"      ---- double quote
+\\      ---- backslash
+\n      ---- newline
+\r      ---- carriage return
+\t      ---- tab 
+\b      ---- word boundary
+\f      ---- form feed 
+
+
+//CONCATENATING STRINGS WITH PLUS OPERATOR
+const myStr = "This is the first." + " This is the second.";
+//This is the first. This is the second.
+
+
+//CONCATENATING STRINGS WITH THE PLUS EQUALS OPERATOR
+let myStr = "This is the first. "; 
+myStr += "This is the second."; 
+//This is the first. This is the second.
+
+
+//CONSTRUCTING STRINGS WITH VARIABLES
