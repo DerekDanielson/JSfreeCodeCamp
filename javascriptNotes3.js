48f730ac2665979baa355d4e9107b42962e34107
Derek Danielson <derekdanielson84@gmail.com>
2022-04-29 11:48:01 -0600

    More JS notes

--- javascriptNotes3.js
+++ javascriptNotes3.js
@@ -1,1 +1,18 @@
-//CONSTRUCTING STRINGS WITH VARIABLES
+//APPENDING VARIABLES TO STRINGS
+const anAdjective = "interesting";
+let ourStr = "coding is ";
+ourStr += anAdjective;
+//ourStr now = "coding is interesting"
+
+
+//FIND LENGTH OF A STRING ******************************
+let lastNameLength = 0;
+const lastName = "Simpson";
+lastNameLength = lastName.length;
+
+console.log(lastNameLength);
+//lastNameLength is now 7
+
+
+//
+
